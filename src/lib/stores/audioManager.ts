718f0c35ae9fad29135b75432eb8fe77a441fd36
Mohamed Al Glawi <mohamed.alglawi@gmail.com>
2025-07-19 16:21:41 +0200

    feat: enhance audio player

--- src/lib/stores/audioManager.ts
+++ src/lib/stores/audioManager.ts
@@ -1,38 +1,70 @@
 import { writable } from 'svelte/store';
 
 class AudioInstance {
-	private audio: HTMLAudioElement;
+	private audio?: HTMLAudioElement;
 	private id: string;
+	protected path: string;
+	private loop: boolean;
 
 	constructor(id: string, loop: boolean = false) {
 		this.id = id;
-		this.audio = new Audio(`/audios/${id}.mp3`);
-		this.audio.loop = loop;
+		this.path = `/audios/${id}.mp3`;
+		this.loop = loop;
+
+	}
+
+	fetch() {
+		this.audio = new Audio(this.path);
+		this.audio.loop = this.loop;
 	}
 
 	play() {
-		this.audio.play().catch(err => console.error(`Error playing ${this.id}:`, err));
+		if (this.audio != undefined) {
+			this.audio.play().catch(err => console.error(`Error playing ${this.id}:`, err));
+		}
 	}
 
 	pause() {
-		this.audio.pause();
+		if (this.audio != undefined) {
+			this.audio.pause();
+		}
 	}
 
 	stop() {
-		this.audio.pause();
-		this.audio.currentTime = 0;
+		if (this.audio != undefined) {
+			this.audio.pause();
+			this.audio.currentTime = 0;
+		}
 	}
 
 	setLoop(loop: boolean) {
-		this.audio.loop = loop;
+		if (this.audio != undefined) {
+			this.audio.loop = loop;
+		}
 	}
 
-	isPlaying() {
-		return !this.audio.paused;
+	isPlaying(): boolean | undefined {
+		if (this.audio != undefined) {
+			return !this.audio.paused;
+		}
 	}
 
 	getId() {
 		return this.id;
+	}
+
+	getDuration(): number | undefined {
+		if (this.audio !== undefined)  {
+			return this.audio?.duration;
+
+		}
+	}
+}
+
+class AudioUrlInstance extends AudioInstance {
+	constructor(id: string, url: string, loop: boolean) {
+		super(id, loop);
+		this.path = url;
 	}
 }
 
@@ -42,11 +74,16 @@ export class AudioManager {
 	/**
 	 * Play an audio by id (creates it if doesn't exist)
 	 */
-	play(id: string, loop: boolean = false) {
+	play(id: string, loop: boolean = false, url?: string) {
 		let player = this.players.get(id);
 
 		if (!player) {
-			player = new AudioInstance(id, loop);
+			if (url !== undefined) {
+				player = new AudioUrlInstance(id, url, loop)
+			} else {
+				player = new AudioInstance(id, loop);
+			}
+			player.fetch();
 			this.players.set(id, player);
 		} else {
 			player.setLoop(loop);
@@ -54,6 +91,7 @@ export class AudioManager {
 
 		player.play();
 	}
+
 
 	/**
 	 * Pause a specific audio by id
@@ -88,6 +126,10 @@ export class AudioManager {
 	 */
 	isPlaying(id: string): boolean {
 		return this.players.get(id)?.isPlaying() ?? false;
+	}
+
+	getPlayerInstance(id: string): AudioInstance | undefined {
+		return this.players.get(id);
 	}
 }
 
